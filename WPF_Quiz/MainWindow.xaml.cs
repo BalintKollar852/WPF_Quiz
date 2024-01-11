6f0d78dc3c195570fdc6d2c2420dbdccc2a742db
Torrente <71377802+BalintKollar852@users.noreply.github.com>
2024-01-11 09:14:33 +0100

    Basics code

--- WPF_Quiz/MainWindow.xaml.cs
+++ WPF_Quiz/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +22,35 @@ namespace WPF_Quiz
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static int Type_number;
+        public static string Name;
         public MainWindow()
         {
             InitializeComponent();
+            foreach (string sor in File.ReadAllLines(@"players.txt"))
+            {
+                string[] s = sor.Split(';');
+                Name_input.Items.Add(s[0]);
+            }
+        }
+        private void ComboBoxName_TextChanged(object sender, EventArgs e)
+        {
+            if (Name_input.Text.Length > 0 && Regex.IsMatch(Name_input.Text, @"^[a-záéúőóüö A-ZZÁÉÚŐÓÜÖÍ]+$"))
+            {
+                GameStart_Button.IsEnabled = true;
+            }
+            else
+            {
+                GameStart_Button.IsEnabled = false;
+            }
+        }
+        public void GameClick(object sender, RoutedEventArgs e)
+        {
+            Name = Name_input.Text;
+            if (category1_button.IsChecked == true) Type_number = 0;
+            else if (category2_button.IsChecked == true) Type_number = 1;
+            else if (category3_button.IsChecked == true) Type_number = 2;
+            MainFrame.Content = new Quiz();
         }
     }
 }
---
+++ WPF_Quiz/Quiz.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Quiz
+{
+    /// <summary>
+    /// Interaction logic for Quiz.xaml
+    /// </summary>
+    class Players
+    {
+        public string Name { get; private set; }
+        public int Category1GuessedNumber { get; private set; }
+        public int Category1QuestionsNumber { get; private set; }
+        public int Category2GuessedNumber { get; private set; }
+        public int Category2QuestionsNumber { get; private set; }
+        public int Category3GuessedNumber { get; private set; }
+        public int Category3QuestionsNumber { get; private set; }
+        public Players(string line)
+        {
+            string[] s = line.Split(';');
+            Name = s[0];
+            Category1GuessedNumber = Convert.ToInt32(s[1]);
+            Category1QuestionsNumber = Convert.ToInt32(s[2]);
+            Category2GuessedNumber = Convert.ToInt32(s[3]);
+            Category2QuestionsNumber = Convert.ToInt32(s[4]);
+            Category3GuessedNumber = Convert.ToInt32(s[5]);
+            Category3QuestionsNumber = Convert.ToInt32(s[6]);
+        }
+    }
+    public partial class Quiz : Page
+    {
+        public Quiz()
+        {
+            InitializeComponent();
+        }
+    }
+}
